4a647ae7d10e66253d7560a99cac76748b1a7c54
robertlugana <brentthopmson@gmail.com>
2025-03-08 10:10:54 -0600

    NHH

--- app/components/AdminLogin.tsx
+++ app/components/AdminLogin.tsx
@@ -1,6 +1,9 @@
+// /components/AdminLogin.tsx
+
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { useUser } from '../UserContext';
+import { User } from '../types'; // Ensure User is imported
 
 interface AdminLoginProps {
   setLoggedInAdmin: React.Dispatch<React.SetStateAction<string | null>>;
@@ -10,7 +13,7 @@ interface AdminLoginProps {
 const AdminLogin: React.FC<AdminLoginProps> = ({ setLoggedInAdmin, setUsers }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const { fetchAdminData, fetchAllUsers, loading } = useUser();
+  const { setAdmin, fetchAllUsers, fetchAdminData, loading } = useUser();
   const router = useRouter();
 
   const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -22,12 +25,13 @@ const AdminLogin: React.FC<AdminLoginProps> = ({ setLoggedInAdmin, setUsers }) =
     }
 
     try {
-      await fetchAdminData(username, password); // Call the fetchAdminData method for login
-      setLoggedInAdmin(username); // Set the logged-in admin in the parent component
-      fetchAllUsers(); // Fetch users once admin logs in
-      router.push('/admin'); // Redirect the user to the admin page after successful login
+      await fetchAdminData(username, password);
+      fetchAllUsers();
+      setLoggedInAdmin(username);
+      sessionStorage.setItem("loggedInAdmin", username);
+      router.push('/admin');
     } catch (error) {
-      console.error('Error logging in:', error); // Handle login errors
+      console.error('Error logging in:', error);
     }
   };
 
@@ -36,37 +40,31 @@ const AdminLogin: React.FC<AdminLoginProps> = ({ setLoggedInAdmin, setUsers }) =
       <div className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-lg max-w-md w-full">
         <h2 className="text-3xl font-semibold text-gray-900 dark:text-gray-100 mb-4">Admin Login</h2>
         <form onSubmit={handleLogin} className="space-y-4">
-          <div>
-            <input
-              type="text"
-              name="username"
-              placeholder="Username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              className="w-full p-2 border border-gray-300 rounded-lg dark:bg-gray-700 dark:text-gray-100"
-              required
-            />
-          </div>
-          <div>
-            <input
-              type="password"
-              name="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full p-2 border border-gray-300 rounded-lg dark:bg-gray-700 dark:text-gray-100"
-              required
-            />
-          </div>
-          <div>
-            <button
-              type="submit"
-              className="w-full bg-blue-600 text-white px-6 py-3 rounded-full text-lg hover:bg-blue-500 transition"
-              disabled={loading}
-            >
-              {loading ? 'Logging in...' : 'Login'}
-            </button>
-          </div>
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="w-full p-2 border border-gray-300 rounded-lg dark:bg-gray-700 dark:text-gray-100"
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full p-2 border border-gray-300 rounded-lg dark:bg-gray-700 dark:text-gray-100"
+            required
+          />
+          <button
+            type="submit"
+            className="w-full bg-blue-600 text-white px-6 py-3 rounded-full text-lg hover:bg-blue-500 transition"
+            disabled={loading}
+          >
+            {loading ? 'Logging in...' : 'Login'}
+          </button>
         </form>
       </div>
     </div>
